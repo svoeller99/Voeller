26309dba064962691ab4efc1f9cce97f4118668f
Sean Voeller <sean.voeller@gmail.com>
2011-07-03 19:08:54 -0700

    Adding scaffold for Post class

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 VoellerStuff::Application.routes.draw do
+  resources :posts
+
   get "welcome/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ VoellerStuff::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
+  root :to => "posts#index"
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110704020641) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stories", :force => true do |t|
+    t.string   "name"
+    t.string   "link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
